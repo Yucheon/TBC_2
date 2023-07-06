30cc1dbf42331ed51aab0a917dcb4d02387eb10a
Yucheon <103819552+Yucheon@users.noreply.github.com>
2023-07-06 22:56:39 +0900

    07/06 study

---
+++ Chapter09_08/Chapter09_08.cpp
@@ -0,0 +1,54 @@
+#include <iostream> //9.8 형변환을 오버로딩 하기
+
+using namespace std;
+
+class Cents
+{
+private:
+	int m_cents;
+public:
+	Cents(int cents = 0)
+	{
+		m_cents = cents;
+	}
+
+	int getCents() { return m_cents; }
+	void setCetns(int cents) { m_cents = cents; }
+
+	operator int()
+	{
+		cout << "cast here" << endl;
+		return m_cents;
+	}
+};
+
+class Dollar
+{
+private:
+	int m_dollars = 0;
+public:
+	Dollar(const int& input) : m_dollars(input) {}
+
+	operator Cents()
+	{
+		return Cents (m_dollars*100);
+	}
+
+};
+void printInt(const int& value)
+{
+	cout << value << endl;
+}
+
+int main()
+{
+	Dollar dol(2);
+
+	Cents cents = dol;
+
+	Cents cents(7);
+
+	printInt(cents);
+
+	return 0;
+}
