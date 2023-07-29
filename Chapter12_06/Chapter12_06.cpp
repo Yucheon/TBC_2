f2adc80f8651fb4282af440ca0ef13b8e97e024a
Yucheon <103819552+Yucheon@users.noreply.github.com>
2023-07-29 22:56:55 +0900

    07/29 study

---
+++ Chapter12_06/Chapter12_06.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Animal
+{
+protected:
+	string m_name;
+
+public:
+	Animal(std::string name)
+		:m_name(name)
+	{}
+
+public:
+	string getName() { return m_name; }
+
+	virtual void speak() const = 0;
+
+};
+
+void Animal::speak() const
+{
+	cout << m_name << "???" << endl;
+}
+
+class Cat :public Animal
+{
+public:
+	Cat(string name)
+		:Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << "Meow" << endl;
+	}
+};
+
+class Dog :public Animal
+{
+public:
+	Dog(string name)
+		:Animal(name)
+	{}
+
+	void speak() const
+	{
+		cout << m_name << "Woof" << endl;
+	}
+};
+
+int main()
+{
+}
