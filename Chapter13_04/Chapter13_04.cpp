3c5bb61680a1dd1954090455d19da67ca9575efc
Yucheon <103819552+Yucheon@users.noreply.github.com>
2023-08-12 19:21:01 +0900

    08/12 study

---
+++ Chapter13_04/Chapter13_04.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "Storage.h"
+
+using namespace std;
+
+template<typename T>
+T  getMax(T x, T y)
+{
+	return (x > y) ? x : y;
+}
+
+template<>
+char getMax(char x, char y)
+{
+	cout << "Warning: conparing chars" << endl;
+
+	return (x > y) ? x : y;
+}
+int main()
+{
+	cout << getMax('a', 'b') << endl;
+
+	return 0;
+}
